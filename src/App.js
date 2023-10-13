9325304e49330fb093073143480a9ed119f3ec85
bhaskar <bhaskardey407@gmail.com>
2023-10-13 23:51:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AgeCalculator from './components/AgeCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <AgeCalculator />
+      </main>
+    </div>
+  );
+}
+
+export default App;
